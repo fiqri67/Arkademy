298f1a5cb60c76af48d011a45ea0d7cfc9698f07
fiqri67 <32330871+fiqri67@users.noreply.github.com>
2019-11-16 21:45:50 +0700

    Add files via upload

---
+++ 6a.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Arkademy
+{
+    #region Cashier
+    public class Cashier
+    {
+        #region Member Variables
+        protected int _id;
+        protected string _Cashier;
+        #endregion
+        #region Constructors
+        public Cashier() { }
+        public Cashier(string Cashier)
+        {
+            this._Cashier=Cashier;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id
+        {
+            get {return _id;}
+            set {_id=value;}
+        }
+        public virtual string Cashier
+        {
+            get {return _Cashier;}
+            set {_Cashier=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ 6a.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.0.1
+ */
+
+/**
+ * Database `arkademy`
+ */
+
+/* `arkademy`.`cashier` */
+$cashier = array(
+  array('Cashier' => 'Raisa Adriana','Product' => 'Cake','Category' => 'food','Price' => '20000'),
+  array('Cashier' => 'Fiqri Yusril Rizal','Product' => 'Onde','Category' => 'food','Price' => '50000'),
+  array('Cashier' => 'Pevita Pearce','Product' => 'Latte','Category' => 'drink','Price' => '10000')
+);
